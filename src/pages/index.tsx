aa95a76bfbbaf612c0ca4436761774825edec5e8
sanjiv <sanjivgautamofficial@gmail.com>
2024-09-21 00:50:16 +0545

    feat: works on header 111 part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { DeviceType, Navbar } from "@/components/common/Navbar";
-import { Header109 } from "@/components/Header109";
+import { Header111 } from "@/components/Header111";
 import { useState } from "react";
 
 export default function Home() {
@@ -10,7 +10,8 @@ export default function Home() {
       <div className="flex flex-col bg-gray-100">
         <Navbar selectedDevice={selectedDevice} setSelectedDevice={setSelectedDevice} />
         <div className="flex justify-center">
-          <Header109 selectedDevice={selectedDevice} />
+          {/* <Header109 selectedDevice={selectedDevice} /> */}
+          <Header111 selectedDevice={selectedDevice} />
         </div>
       </div>
       <div className="min-h-screen"></div>
